67b07ad08fd91689978e4eac26e602b14a6b13c4
Tarik Pribisic <pribajaba@gmail.com>
2020-10-30 23:49:35 +0100

    Animation implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module'
 import { HomeModule } from './modules/home/home.module';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { HomeModule } from './modules/home/home.module';
     SharedModule,
     HomeModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -6,10 +6,10 @@ import { ItemListComponent } from './item-list/item-list.component';
 import { ItemComponent } from './item/item.component';
 
 const routes: Routes = [
-  { path: '', component: ItemListComponent, data : {type : 'tv'}},
-  { path: 'movies', component: ItemListComponent, data : {type : 'movie'}},
-  { path: 'movie/:id', component: ItemComponent, data: {type : 'movie'}},
-  { path: 'tv/:id', component:ItemComponent, data: {type : 'tv'}},
+  { path: '', component: ItemListComponent, data : {type : 'tv', animation:'TVPage' }},
+  { path: 'movies', component: ItemListComponent, data : {type : 'movie', animation:'MoviesPage' }},
+  { path: 'movie/:id', component: ItemComponent, data: {type : 'movie', animation:'MoviesIDPage' }},
+  { path: 'tv/:id', component:ItemComponent, data: {type : 'tv', animation:'TVIDPage' }},
   { path: '**', component:NotfoundComponent }
 ];
 @NgModule({
